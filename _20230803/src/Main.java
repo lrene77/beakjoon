916d61388549fd82686f2050d0bb095ebe08664e
SORA <121392539+lrene77@users.noreply.github.com>
2023-08-12 22:14:34 +0900

    Add files via upload

---
+++ _20230803/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //a와b를 받아 a+b를 출력하는 프로그램
+
+        int a,b;
+        Scanner scn = new Scanner(System.in);
+
+        a = scn.nextInt();
+        b = scn.nextInt();
+
+        System.out.println(a+b);
+
+
+
+    }
+}
