ff76742e7e5a89a6b62adf4bd02d2e333927e838
SORA <121392539+lrene77@users.noreply.github.com>
2023-08-12 22:15:28 +0900

    Add files via upload

---
+++ baekjoon_1001/baekjoon_1001/baekjoon_1001.cpp
@@ -0,0 +1,15 @@
+﻿// 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+
+#include <iostream>
+
+int main()
+{
+    int a, b; //정수형으로 초기화
+
+    std::cin >> a >> b; //입력
+
+    std::cout<< a - b; //출력
+
+
+}
+
