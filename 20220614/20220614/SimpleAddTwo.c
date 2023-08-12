d837e37f97272f01eb2fb05534f38d66045d7ad8
SORA <121392539+lrene77@users.noreply.github.com>
2023-08-12 22:21:11 +0900

    Add files via upload

---
+++ 20220614/20220614/SimpleAddTwo.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main1(void) {
+
+	int num1 = 3;
+	int num2 = 4;
+	int result = num1 + num2;
+
+	printf("덧셈 결과 : %d \n", result);
+	printf("%d + %d = %d \n", num1, num2, result);
+	printf("%d와(과) %d의 합은%d 입니다. \n", num1, num2, result);
+
+	return 0;
+
+}
