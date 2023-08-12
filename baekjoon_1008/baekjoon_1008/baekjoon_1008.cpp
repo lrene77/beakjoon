ddeeef1c230ca488e1099068b75ed9d167979bf0
SORA <121392539+lrene77@users.noreply.github.com>
2023-08-12 22:15:55 +0900

    Add files via upload

---
+++ baekjoon_1008/baekjoon_1008/baekjoon_1008.cpp
@@ -0,0 +1,14 @@
+﻿//두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+
+#include <iostream>
+#include <iomanip> //setprecision을 쓰기위한 선언
+
+int main()
+{   
+    int a, b; // 초기화
+    a < 0; // 범위설정
+    b > 10; // 범위설정
+
+    std::cin >> a >> b; // 입력
+    std::cout << std::setprecision(12)<<(double)a / b; // 출력 setprecision으로 소수점 출력수 제한 및 (double)로 형변환
+}
